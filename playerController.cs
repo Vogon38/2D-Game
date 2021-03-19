5e4e00824c8f13b7896a0dd1e2e804c26d14a8d1
Luana Cristini <luana@thecyborg.xyz>
2021-03-19 01:49:55 -0300

    Add files via upload

---
+++ playerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	private Rigidbody2D		playerRb;
+	
+	public float 			speed;
+	public float			jumpForce;
+		
+	void Start () {
+
+		playerRb = GetComponent<Rigidbody2D>();
+	
+	}
+	
+	void Update () {
+
+		float h =Input.GetAxisRaw("Horizontal");
+		float speedY = playerRb.velocity.y;
+
+		playerRb.velocity = new Vector2(h*speed, speedY);
+
+	}
+}
